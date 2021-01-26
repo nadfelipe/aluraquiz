0b219c8eaa5bcd9c80f940ad5cf25a3aa7b88948
nadfelipe <nadfelipe.s@gmail.com>
2021-01-25 22:27:48 -0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,67 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React, {useState} from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import db from '../db.json';
+import ThemeContext from '../src/contexts/Theme'
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
+const defaultTheme = db.theme;
+
+
+const lightTheme = {
+  "colors": {
+    "primary": "#F2A9A2",
+    "secondary": "#4EA675",
+    "mainBg": "#F0E9D1",
+    "contrastText": "#3E3640",
+    "wrong": "#FF5722",
+    "success": "#4CAF50"
   },
+  "borderRadius": "4px"
 }
 
 export default function App({ Component, pageProps }) {
+  const [theme, updateTheme] = useState(defaultTheme)
+
+  const handleTheme = () => {
+    updateTheme (theme =>{
+      if (theme.colors.primary === '#47BFBF') {
+        return lightTheme;
+      }
+      return defaultTheme;
+    })
+  }
+
   return (
-    <>
-      <GlobalStyle />
+    <ThemeContext.Provider value={{
+      switchTheme: handleTheme,
+    }}>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </ThemeContext.Provider>
   )
 }
---
+++ src/components/SelectTheme/index.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+import React, { useState, useContext } from 'react';
+import ThemeContext from '../../contexts/Theme'
+
+export function ThemeSelect() {
+  const { switchTheme } = useContext(ThemeContext)
+
+return (
+    <CheckBoxWrapper>
+      <CheckBox id="switch-flat" type="checkbox" onClick={switchTheme} />
+      <CheckBoxLabel for="switch-flat"></CheckBoxLabel>
+    </CheckBoxWrapper>
+);
+};
+
+const CheckBoxWrapper = styled.div`
+  position: relative;
+  align-self: center;
+`;
+
+const CheckBoxLabel = styled.label`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 42px;
+  height: 26px;
+  border-radius: 15px;
+  background: ${({ theme }) => theme.colors.primary};
+  cursor: pointer;
+  &::after {
+    content: "";
+    display: block;
+    border-radius: 50%;
+    width: 18px;
+    height: 18px;
+    margin: 3px;
+    background: ${({ theme }) => theme.colors.contrastText};;
+    box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
+    transition: 0.2s;
+  }
+`;
+
+const CheckBox = styled.input`
+  opacity: 0;
+  z-index: 1;
+  border-radius: 15px;
+  width: 42px;
+  height: 26px;
+  &:checked + ${CheckBoxLabel} {
+    background: ${({ theme }) => theme.colors.primary};
+    &::after {
+      content: "";
+      display: block;
+      border-radius: 50%;
+      width: 18px;
+      height: 18px;
+      margin-left: 21px;
+      transition: 0.2s;
+    }
+  }
+`;
